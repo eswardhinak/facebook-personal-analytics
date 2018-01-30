ba85b4cfce201af28d5b8e9400783f2695973501
Eswar Dhinakaran <eswardhinak@gmail.com>
2018-01-29 18:18:16 -0800

    can plot message history

--- messageAnalysis.py
+++ messageAnalysis.py
@@ -1,7 +1,9 @@
 from BeautifulSoup import BeautifulSoup
 import os
 import sys
-import plotly
+import matplotlib.dates as dates
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 def analyzeConversation(soup):
 	allTagsList = soup.html.body.div.findAll(recursive=False)
@@ -36,20 +38,58 @@ def analyzeConversation(soup):
 		date = bsReader.fetch('span', {'class':'meta'})[0].string
 		dateList.append(date)
 
-	print len(userNames), len(dateList)
+	parsedParTags = []
+	for par in parTags:
+		updatedPar = par[3:-4]
+		parsedParTags.append(updatedPar)
+	return dateList, userNames, parsedParTags
 
+def getDateNoTimeStamp(date):
+	return date.split(' at ')[0]
 
+def getDatesWithNumberOfWords(dateList, messagesList):
+	dateToTexts = {}
+	for i in range(0, len(dateList)):
+		date = dateList[i]
+		message = messagesList[i]
+
+		if (date not in dateToTexts):
+			dateToTexts[date] = [message]
+		else:
+			dateToTexts[date].append(message)
+	uniqueDates = dateToTexts.keys()
+	wordNumbers = []
+	for date in dateToTexts:
+		currentDaysWords = dateToTexts[date]
+		totalMessages = 0
+		for message in currentDaysWords:
+			totalMessages += len(message.split())
+		wordNumbers.append(totalMessages)
+	return uniqueDates, wordNumbers
+
+def getDateTimeObject(date):
+	datetime_object = datetime.strptime(date, "%A, %B %d, %Y")
+	return datetime_object
 
 MESSAGES_DIRECTORY = './data/facebook-eswardhinak/messages/'
 for filename in os.listdir(MESSAGES_DIRECTORY):
-	if filename == '1117.html':
-		currentFile = open(MESSAGES_DIRECTORY + filename)
-		soup = BeautifulSoup(currentFile.read())
+	currentFile = open(MESSAGES_DIRECTORY + filename)
+	soup = BeautifulSoup(currentFile.read())
 
-		conversationHeaderTag = soup.html.head.title
+	conversationHeaderTag = soup.html.head.title
+	if (conversationHeaderTag.string.endswith('Aviv Redlich')):
 		print filename
-		analyzeConversation(soup)
+		# sys.exit()
+		dateList, userNames, parTags = analyzeConversation(soup)
+		dateListAscii = list(map(lambda date:date.encode('ascii', 'ignore'), dateList))
+		datesNoTimeStampList = list(map(getDateNoTimeStamp, dateListAscii))
 
+		uniqueDates, wordNumbers = getDatesWithNumberOfWords(datesNoTimeStampList, parTags)
+		uniqueDateObjects = list(map(getDateTimeObject, uniqueDates))
+		uniqueSortedDateObjects, sortedWordCounts = zip(*sorted(zip(uniqueDateObjects, wordNumbers)))
 
-
-
+		# print uniqueSortedDateObjects, sortedWordCounts
+		matplotlibDates = dates.date2num(uniqueSortedDateObjects)
+		plt.plot_date(matplotlibDates, sortedWordCounts, '-o')
+		plt.show()
+		print "Done"
